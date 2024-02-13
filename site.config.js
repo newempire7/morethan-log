811da751ff8df8e906bd98e7c3d52c2496970a51
newempire7 <159776634+newempire7@users.noreply.github.com>
2024-02-13 19:58:10 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -15,6 +15,8 @@ const CONFIG = {
       name: `커뮤니티 단톡방`,
       href: "https://open.kakao.com/o/gOLfMFTc",
     },
+  ],
+   projects: [
     {
       name: `선정된 업체 list`,
       href: "https://open.kakao.com/o/gOLfMFTc",
