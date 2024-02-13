f745006338b486c757b7bf7034dd058139a4ef6c
newempire7 <159776634+newempire7@users.noreply.github.com>
2024-02-13 18:05:05 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -55,7 +55,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "newempire7/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
