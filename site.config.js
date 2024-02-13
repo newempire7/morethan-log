b0afe3380f1abad1305f250d8424b44f0e6f7cae
newempire7 <159776634+newempire7@users.noreply.github.com>
2024-02-13 18:15:30 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -60,12 +60,12 @@ const CONFIG = {
       label: "💬 Utterances",
     },
   },
-  cusdis: {
-    enable: false,
-    config: {
-      host: "https://cusdis.com",
-      appid: "", // Embed Code -> data-app-id value
-    },
+  // cusdis: {
+    //enable: false,
+    //config: {
+     // host: "https://cusdis.com",
+      //appid: "", // Embed Code -> data-app-id value
+   // },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
